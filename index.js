948e924142efca08afb50be6e3dd14bfb801f3fb
Toni Lappalainen <toni.lappalainen@gmail.com>
2020-04-27 14:50:33 +0300

    first

---
+++ index.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const expressEdge = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Post = require("./database/models/Post");
+
+const app = new express();
+
+mongoose
+  .connect("mongodb://localhost:27017/node-blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => "You are now connected to Mongo!")
+  .catch((err) => console.error("Something went wrong", err));
+
+app.use(express.static("public"));
+app.use(expressEdge.engine);
+app.set("views", __dirname + "/views");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+// render edge template 'index' from 'views' folder
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/posts/store", (req, res) => {
+  Post.create(req.body, (error, post) => {
+    res.redirect("/");
+  });
+});
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
