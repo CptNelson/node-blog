67e34ac120fcfacc39121a7ed25d87c70627c679
Toni Lappalainen <toni.lappalainen@gmail.com>
2020-04-28 12:49:48 +0300

    almost done

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
-const path = require("path");
 const expressEdge = require("express-edge");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const Post = require("./database/models/Post");
+const fileUpload = require("express-fileupload");
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
+
+const createPostController = require("./controllers/createPost");
+const homePageController = require("./controllers/homePage");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+const createUserController = require("./controllers/createUser");
+const storeUserController = require("./controllers/storeUser");
+const loginController = require("./controllers/login");
+const loginUserController = require("./controllers/loginUser");
 
 const app = new express();
 
-mongoose
-  .connect("mongodb://localhost:27017/node-blog", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => "You are now connected to Mongo!")
-  .catch((err) => console.error("Something went wrong", err));
-
-app.use(express.static("public"));
-app.use(expressEdge.engine);
-app.set("views", __dirname + "/views");
-app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
+  expressSession({
+    secret: "secret",
   }),
 );
 
-// render edge template 'index' from 'views' folder
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
+mongoose
+  .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
+  .then(() => "You are now connected to Mongo!")
+  .catch((err) => console.error("Something went wrong", err));
 
-app.get("/posts/new", (req, res) => {
-  res.render("create");
-});
+const mongoStore = connectMongo(expressSession);
 
-app.post("/posts/store", (req, res) => {
-  Post.create(req.body, (error, post) => {
-    res.redirect("/");
-  });
-});
+app.use(
+  expressSession({
+    secret: "secret",
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  }),
+);
+
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge.engine);
+app.set("views", __dirname + "/views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const storePost = require("./middleware/storePost");
+
+app.use("/posts/store", storePost);
+
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
+app.get("/auth/login", loginController);
+app.post("/users/login", loginUserController);
+app.get("/auth/register", createUserController);
+app.post("/users/register", storeUserController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
