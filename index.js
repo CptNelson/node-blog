fe60f4a1c76999816577cd6ac745d4a57d306b6e
Toni Lappalainen <toni.lappalainen@gmail.com>
2020-04-29 10:58:27 +0300

    node done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const expressEdge = require("express-edge");
 const express = require("express");
+const edge = require("edge.js");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
@@ -14,6 +15,7 @@ const createUserController = require("./controllers/createUser");
 const storeUserController = require("./controllers/storeUser");
 const loginController = require("./controllers/login");
 const loginUserController = require("./controllers/loginUser");
+const logoutController = require("./controllers/logout");
 
 const app = new express();
 
@@ -44,21 +46,29 @@ app.use(express.static("public"));
 app.use(expressEdge.engine);
 app.set("views", __dirname + "/views");
 
+app.use("*", (req, res, next) => {
+  edge.global("auth", req.session.userId);
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const storePost = require("./middleware/storePost");
+const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require("./middleware/redirectIfAuthenticated");
 
 app.use("/posts/store", storePost);
 
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
-app.get("/posts/new", createPostController);
-app.post("/posts/store", storePostController);
-app.get("/auth/login", loginController);
-app.post("/users/login", loginUserController);
-app.get("/auth/register", createUserController);
-app.post("/users/register", storeUserController);
+app.get("/posts/new", auth, createPostController);
+app.post("/posts/store", auth, storePost, storePostController);
+app.get("/auth/login", redirectIfAuthenticated, loginController);
+app.post("/users/login", redirectIfAuthenticated, loginUserController);
+app.get("/auth/register", redirectIfAuthenticated, createUserController);
+app.post("/users/register", redirectIfAuthenticated, storeUserController);
+app.get("/auth/logout", redirectIfAuthenticated, logoutController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
